18a4901be4dd200d8343274529472c3b96bce80d
jhong40 <jhong40@gmail.com>
2021-11-27 20:14:00 -0500

    Update slackpostmsg.py

--- slackpostmsg.py
+++ slackpostmsg.py
@@ -1,5 +1,5 @@
 import requests
 endpoint = "https://slack.com/api/chat.postMessage"
-data = {"channel":"C02P5C5FCBT", "text": "123 great goose turkey....blah blah ....test from python"}
-headers = {"Authorization": "Bearer xoxb-767624982948-2771827764917-1uLpXFx37dCU2A3yrsthDRNa"}
+data = {"channel":"Cxxxx", "text": "123 great goose turkey....blah blah ....test from python"}
+headers = {"Authorization": "Bearer xoxb-xxxx"}
 print(requests.post(endpoint, data=data, headers=headers).json())
